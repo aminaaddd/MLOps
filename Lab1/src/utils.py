9360ab40181673aec71224d00f69bf8a6d68e195
ad.m.na <amina.addi@yahoo.com>
2025-10-16 22:34:25 +0200

    Update divide function logic.

--- Lab1/src/utils.py
+++ Lab1/src/utils.py
@@ -14,11 +14,18 @@ def multiply(*args):
     return first
 
 def divide(*args):
+    if len(args) == 0:
+        raise IndexError("one argument required, but none given")
+    
     first = args[0]
-    if first == 0:
-        raise ValueError("Cannot divide by zero")
+    if len(args) == 1:
+        return first
+    
     for arg in args[1:]:
         if arg == 0:
             raise ValueError("Cannot divide by zero")
         first /= arg
+        
+    if isinstance(first, float):
+        return round(first, 4)
     return first
