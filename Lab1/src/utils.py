7ed445b79beeb0187143b5f9660331cc0ec48dd5
ad.m.na <amina.addi@yahoo.com>
2025-10-16 20:07:03 +0200

    Add multiply and divide functions.

--- Lab1/src/utils.py
+++ Lab1/src/utils.py
@@ -7,3 +7,18 @@ def substract(*args):
         fist -= arg 
     return fist
 
+def multiply(*args):
+    first = args[0]
+    for arg in args[1:]:
+        first *= arg
+    return first
+
+def divide(*args):
+    first = args[0]
+    if first == 0:
+        raise ValueError("Cannot divide by zero")
+    for arg in args[1:]:
+        if arg == 0:
+            raise ValueError("Cannot divide by zero")
+        first /= arg
+    return first
