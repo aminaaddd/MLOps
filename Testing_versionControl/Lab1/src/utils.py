ddb76f5ce64cc8fc933d72af9b91f94eaf46e021
ad.m.na <amina.addi@yahoo.com>
2025-10-18 19:05:37 +0200

    Resole flake8 conflicts.

--- Testing_versionControl/Lab1/src/utils.py
+++ Testing_versionControl/Lab1/src/utils.py
@@ -1,11 +1,13 @@
 def add(*args):
     return sum(args)
 
+
 def substract(*args):
-    fist = args[0]
+    first = args[0]
     for arg in args[1:]:
-        fist -= arg 
-    return fist
+        first -= arg
+    return first
+
 
 def multiply(*args):
     first = args[0]
@@ -13,19 +15,20 @@ def multiply(*args):
         first *= arg
     return first
 
+
 def divide(*args):
     if len(args) == 0:
         raise IndexError("one argument required, but none given")
-    
+
     first = args[0]
     if len(args) == 1:
         return first
-    
+
     for arg in args[1:]:
         if arg == 0:
             raise ValueError("Cannot divide by zero")
         first /= arg
-        
+
     if isinstance(first, float):
         return round(first, 4)
     return first
